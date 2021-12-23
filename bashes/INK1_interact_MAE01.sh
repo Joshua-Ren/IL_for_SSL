82309f6314ea721bb01d8f8e09544bf0eab01442
Joshua-Ren <renyi.joshua@gmail.com>
2021-12-23 10:10:41 -0800

    INK1 interact

---
+++ bashes/INK1_interact_MAE01.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -A NLP-CDT-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --time=36:00:00
+#SBATCH --job-name=mae
+#SBATCH --output=./logs/test.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32GB
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+
+. /etc/profile.d/modules.sh
+module purge
+module load rhel8/default-amp
+module load python/3.8 cuda/11.0 cudnn/8.0_cuda-11.1
+
+source /home/sg955/egg-env/bin/activate
+
+cd /home/sg955/GitWS/IL_for_SSL/
+
+srun python INK1_interact_MAE.py --run_name hopetowork
