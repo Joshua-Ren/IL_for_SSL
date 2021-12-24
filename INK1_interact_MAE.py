f7113209819f7573d39b62068e32256997cb0e3f
Joshua-Ren <renyi.joshua@gmail.com>
2021-12-23 18:04:49 -0800

    val as train

--- INK1_interact_MAE.py
+++ INK1_interact_MAE.py
@@ -80,7 +80,7 @@ val_dataset = torchvision.datasets.ImageFolder(
         transforms.ToTensor(),
         normalize,
     ]))
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=16, pin_memory=True)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, pin_memory=True)
 TRACK_TVX = wandb_gen_track_x(train_loader,val_loader)
 TRACK_TVX = TRACK_TVX.to(device)
