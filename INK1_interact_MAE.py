c13ee17565914f32b47ce96fcee2804de1292b62
Joshua-Ren <renyi.joshua@gmail.com>
2021-12-23 16:28:39 -0800

    val as train

--- INK1_interact_MAE.py
+++ INK1_interact_MAE.py
@@ -59,8 +59,8 @@ if not os.path.exists(save_path):
     
 # ======== Get Dataloader and tracking images ===================
 DATA_PATH = '/home/sg955/rds/hpc-work/ImageNet/'
-traindir = os.path.join(DATA_PATH, 'val.zip')
-valdir = os.path.join(DATA_PATH, 'val.zip')
+traindir = os.path.join(DATA_PATH, 'val')
+valdir = os.path.join(DATA_PATH, 'val')
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
 
 train_dataset = torchvision.datasets.ImageFolder(
