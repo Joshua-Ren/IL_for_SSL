8f976a50295dde7975f4942f35d8fa86a71c1805
Joshua-Ren <renyi.joshua@gmail.com>
2021-12-25 21:29:01 -0800

    try multigpu

--- bashes/test_multipleGPU.sh
+++ bashes/test_multipleGPU.sh
@@ -2,4 +2,4 @@ export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
 #!/bin/bash
-python -m torch.distributed.launch --nproc_per_node=4 --master_port 10086 INK1_interact_MAE.py (--run_name testMulti --enable_amp)
+python -m torch.distributed.launch --nproc_per_node=4 --master_port 10086 INK1_interact_MAE.py --run_name testMulti --enable_amp
