7f45bf107f5eb16c5e0e570d4b2fb4becda8e033
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-09 17:10:38 -0800

    inital

--- bashes/ImgNet_finetune_ckp_4GPU.sh
+++ bashes/ImgNet_finetune_ckp_4GPU.sh
@@ -20,5 +20,5 @@ source /home/sg955/egg-env/bin/activate
 cd /home/sg955/GitWS/IL_for_SSL/
 
 srun python -m torch.distributed.launch --nproc_per_node=4 --master_port 10086 ImgNet_finetune_multiGPU.py --enable_amp \
---run_name scratch_4GPU --dataset tiny --modelsize base \
+--run_name 4GPU --dataset imagenet --modelsize base \
 --loadrun basetry_4GPU_1kbs --loadep ep50
