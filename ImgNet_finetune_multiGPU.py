f488ba41ab6c79cef9064a788f6ec1bd701214b1
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-15 11:25:40 -0800

    inital

--- ImgNet_finetune_multiGPU.py
+++ ImgNet_finetune_multiGPU.py
@@ -174,12 +174,7 @@ def main():
         save_path = base_folder+'results/'+args.proj_path+'/'+run_name
         if not os.path.exists(save_path):
             os.makedirs(save_path)
-            
-    for i, (x, _) in enumerate(train_loader):
-            print(x[0])
-            print(x[0].mean())
-            print(x[0].mean())
-            break
+
     # ================= Train the model ===========================
     for g in range(args.epochs):
         train(train_loader, encoder, optimizer, g)
